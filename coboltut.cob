bc387f088fdb1ecf95cbf341d27db7bb24a81242
bkd <contact@bkd.lol>
2026-09-01 20:40:19 -0500

    init

---
+++ coboltut.cob
@@ -0,0 +1,42 @@
+       >>SOURCE FORMAT FREE
+IDENTIFICATION DIVISION.
+PROGRAM-ID. coboltut.
+AUTHOR. Lavender Persson.
+DATE-WRITTEN. September 1st 2026.
+ENVIRONMENT DIVISION.
+
+DATA DIVISION.
+FILE SECTION.
+WORKING-STORAGE SECTION.
+01 UserName PIC X(30) VALUE "You".
+01 Num1    PIC 9 VALUE ZEROS.
+01 Num2    PIC 9 VALUE ZEROS.
+01 Total   Pic 99 VALUE 0.
+01 SSNum.
+       02 SSArea   PIC 999.
+       02 SSGroup  PIC 99.
+       02 SSSerial PIC 9999.
+01 PIVALUE CONSTANT AS 3.14.
+*> Figurative constants:
+*> ZEROS; can also be replaced with ZERO or ZEROES
+*> SPACE or SPACES
+*> HIGH-VALUES
+*> LOW-VALUES
+
+PROCEDURE DIVISION.
+DISPLAY "What is your name " WITH NO ADVANCING
+ACCEPT UserName
+DISPLAY "Hello " UserName
+
+MOVE ZERO TO UserName
+DISPLAY UserName
+DISPLAY "Enter 2 values to sum "
+ACCEPT Num1
+ACCEPT Num2
+COMPUTE Total = Num1 + Num2
+DISPLAY Num1 " + " Num2 " = " Total
+DISPLAY "Enter your SSN "
+ACCEPT SSNum
+DISPLAY "Area " SSArea
+
+STOP RUN.
